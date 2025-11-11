0d2d7be4ca9183fe033836c2200662baa5cf1ea1
pixelprotest <jhooghoudt@gmail.com>
2025-11-11 14:43:48 +0000

    fix: double check true availability of name with project url

--- namecheck/utils.py
+++ namecheck/utils.py
@@ -29,7 +29,51 @@ def get_all_package_names(sources):
     print(f"\nFound {len(package_names)} unique package names across all sources.", file=sys.stderr)
     return dict(package_names)
 
+def get_sources_for_name(name, all_names_with_sources) -> str:
+    """
+    Returns the sources for a given name.
+    """
+    normalized_name = name.lower()
+    sources = ", ".join(sorted(list(all_names_with_sources[normalized_name])))
+    return sources
 
+def is_name_taken_global_index(name, all_names_with_sources) -> bool:
+    """
+    Checks the global index for a given name
+    Gives better overview, but might be cached and outdated.
+    """
+    normalized_name = name.lower()
+    found = True if normalized_name in all_names_with_sources else False
+    return found
+
+def is_name_taken_project_url(name) -> bool:
+    """
+    Instead of checking the global index, we check the project URL directly.
+    Only used as a secondary check to make sure the name _is_ really available, 
+    not just available in the cached global index.
+    """
+    url = f"https://pypi.org/project/{name}/"
+    response = requests.get(url, timeout=30)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+    
+def get_close_matches(name, all_names_with_sources) -> list:
+    """
+    Returns a list of close matches for a given name.
+    """
+    name_norm = name.lower()
+    # Find and display close matches
+    matches = difflib.get_close_matches(name_norm, 
+                                        all_names_with_sources.keys(), 
+                                        n=5, 
+                                        cutoff=0.8)
+    ## if the exact name was found, remove it from 
+    ## the "matches" list to avoid redundancy.
+    if name_norm in matches:
+        matches.remove(name_norm)
+    return matches
 
 def check_name_availability(name, all_names_with_sources):
     """
@@ -38,23 +82,25 @@ def check_name_availability(name, all_names_with_sources):
     normalized_name = name.lower()
     
     print("-" * 50)
+
+    exact_match = is_name_taken_global_index(name, all_names_with_sources)
     
     # Check for an exact match
-    if normalized_name in all_names_with_sources:
-        sources = ", ".join(sorted(list(all_names_with_sources[normalized_name])))
+    if exact_match:
+        sources = get_sources_for_name(name, all_names_with_sources)
         print(f"❌ Exact match for '{name}' found on: {sources}.")
     else:
-        print(f"✅ The name '{name}' appears to be available!")
-
-    # Find and display close matches
-    close_matches = difflib.get_close_matches(
-        normalized_name, all_names_with_sources.keys(), n=5, cutoff=0.8
-    )
+        ## in this case, it _could_ mean the name is available, but
+        ## the cachec might be outdated, so lets do a direct url check
+        ## to make sure
+        is_taken = is_name_taken_project_url(name)
+        if is_taken:
+            print(f"❌ The name '{name}' appears to be unavailable.")
+        else:
+            print(f"✅ The name '{name}' appears to be available!")
     
-    # If the exact name was found, remove it from the "close matches" list to avoid redundancy.
-    if normalized_name in close_matches:
-        close_matches.remove(normalized_name)
-        
+    close_matches = get_close_matches(name, all_names_with_sources)
+    ## if there are close matches, display them
     if close_matches:
         print("\n⚠️  Found closely matching package names:")
         for match in close_matches:
@@ -62,3 +108,4 @@ def check_name_availability(name, all_names_with_sources):
             print(f"   - {match} (on: {sources})")
             
     print("-" * 50)
+
